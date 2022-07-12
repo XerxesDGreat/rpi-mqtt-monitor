afb03644093bccc92655a4d88a090e71abba8e58
Josh Wickham <josh@i-josh.com>
2022-07-12 09:45:57 -0700

    Add logs to metric fetchers; add dry-run mode

--- src/rpi-cpu2mqtt.py
+++ src/rpi-cpu2mqtt.py
@@ -12,6 +12,7 @@ import config
 dictConfig(config.log_config)
 
 import json
+import logging
 import os
 import paho.mqtt.client as paho
 import socket
@@ -24,6 +25,9 @@ hostname = socket.gethostname()
 
 
 def check_used_space(path):
+    logging.debug("check_used_space")
+    if config.dry_run:
+        return 1000
     st = os.statvfs(path)
     free_space = st.f_bavail * st.f_frsize
     total_space = st.f_blocks * st.f_frsize
@@ -32,6 +36,9 @@ def check_used_space(path):
 
 
 def check_cpu_load():
+    logging.debug("check_cpu_load")
+    if config.dry_run:
+        return 2
     # bash command to get cpu load from uptime command
     p = subprocess.Popen("uptime", shell=True, stdout=subprocess.PIPE).communicate()[0]
     cores = subprocess.Popen("nproc", shell=True, stdout=subprocess.PIPE).communicate()[0]
@@ -42,6 +49,9 @@ def check_cpu_load():
 
 
 def check_voltage():
+    logging.debug("check_voltage")
+    if config.dry_run:
+        return 3
     try:
         full_cmd = "vcgencmd measure_volts | cut -f2 -d= | sed 's/000//'"
         voltage = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
@@ -52,6 +62,9 @@ def check_voltage():
 
 
 def check_swap():
+    logging.debug("check_swap")
+    if config.dry_run:
+        return 40
     full_cmd = "free -t | awk 'NR == 3 {print $3/$2*100}'"
     swap = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
     swap = round(float(swap.decode("utf-8").replace(",", ".")), 1)
@@ -59,6 +72,9 @@ def check_swap():
 
 
 def check_memory():
+    logging.debug("check_memory")
+    if config.dry_run:
+        return 50
     full_cmd = "free -t | awk 'NR == 2 {print $3/$2*100}'"
     memory = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
     memory = round(float(memory.decode("utf-8").replace(",", ".")))
@@ -66,6 +82,9 @@ def check_memory():
 
 
 def check_cpu_temp():
+    logging.debug("check_cpu_temp")
+    if config.dry_run:
+        return 60
     full_cmd = "cat /sys/class/thermal/thermal_zone*/temp 2> /dev/null | sed 's/\(.\)..$//' | tail -n 1"
     try:
         p = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
@@ -76,17 +95,26 @@ def check_cpu_temp():
 
 
 def check_sys_clock_speed():
+    logging.debug("check_sys_clock_speed")
+    if config.dry_run:
+        return 7
     full_cmd = "awk '{printf (\"%0.0f\",$1/1000); }' </sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq"
     return subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
 
 
 def check_uptime():
+    logging.debug("check_uptime")
+    if config.dry_run:
+        return 80
     full_cmd = "awk '{print int($1/3600/24)}' /proc/uptime"
     return int(subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0])
 
 def check_model_name():
-   full_cmd = "cat /proc/cpuinfo | grep Model | sed 's/Model.*: //g'"
-   return subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].decode("utf-8") 
+    logging.debug("check_model_name")
+    if config.dry_run:
+        return "pi 4"
+    full_cmd = "cat /proc/cpuinfo | grep Model | sed 's/Model.*: //g'"
+    return subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].decode("utf-8") 
 
 
 def config_json(what_config):
